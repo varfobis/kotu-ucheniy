d56cf208c9bf69950da988595c2f540cab52a2aa
varfobis <35549284+varfobis@users.noreply.github.com>
2024-06-13 17:01:43 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import About from './pages/About';
+import Home from './pages/Home';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
